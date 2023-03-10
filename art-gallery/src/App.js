560e541ee418fc4e8dd914d9ee27b4869434692d
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-03-09 19:48:55 -0800

    created buttonbar.js

--- art-gallery/src/App.js
+++ art-gallery/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {useState, useEffect} from 'react'
+import { Gallery } from '.Gallery.js'
+import { ButtonBar } from '.ButtonBar.js'
 
 function App(){
   let [artId, setArtId] = useState(12720)
   let [data, setData] = useState({})
-  
+
   useEffect(() => {
       document.title='Welcome to Artworld'
       fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
@@ -12,9 +14,29 @@ function App(){
       .then(resData => setData(resData))
   }, [artId])
 
+// in App.js
+// send this function down to <ButtonBar />
+const handleIterate = (e) => {
+  setArtId(artId + Number(e.target.value))
+}
+
+function Gallery(props){
+  return (
+      <div style={{'width': '100%'}}>
+          <img src={props.objectImg} alt={props.title} />
+          <p>{props.artist}</p>
+      </div>
+  )
+}
 
 
-  
+return (
+  <div>
+      <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
+      <buttonBar handleIterate={handleIterate} />
+
+  </div>
+)
 }
 
 
---
+++ art-gallery/src/Gallery.js
@@ -0,0 +1,8 @@
+function Gallery(props){
+    return (
+        <div style={{'width': '100%'}}>
+            <img src={props.objectImg} alt={props.title} />
+            <p>{props.artist}</p>
+        </div>
+    )
+}
